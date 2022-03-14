99325a7d204445c9857b00f106d9f304829ee73a
John <1@1.com>
2022-03-14 15:10:15 -0400

    Sorted out some filepath conflicts that were precluding unit tests, unfortunately this nuke the old test I wrote

---
+++ RSSSorterTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RSSSorter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSSorter.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
