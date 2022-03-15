3fd7d9e584acff4192206c584bd038f501873a3d
Magister Machinis <30908581+Magister-Machinis@users.noreply.github.com>
2022-03-15 19:01:31 -0400

    Update ProgramTests.cs
    initial test rig set up

--- RSSSorterTests/ProgramTests.cs
+++ RSSSorterTests/ProgramTests.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RSSSorter;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,19 @@ namespace RSSSorter.Tests
         [TestMethod()]
         public void MainTest()
         {
-            throw new NotImplementedException();
+            string[] rssurls = { "https://www.google.com/alerts/feeds/05999718615320080555/4574872169044351658", "https://krebsonsecurity.com/feed/", "https://www.google.com/alerts/feeds/05999718615320080555/11815055537862344919" };
+            Directory.CreateDirectory(@".\input");
+            File.WriteAllText(@".\input\rssurls.txt", rssurls.ToString());
+            Directory.CreateDirectory(@".\output");
+            File.WriteAllText(@".\discard.txt", "game");
+            File.WriteAllText(@".\highval.txt", "hacked");
+            RSSSorter.Program.Main(new string[] { @".\input",@".\highval.txt",@".\discard.txt",@".\output","30" });
+            RSSSorter.Program.Main(new string[] { @".\input",@".\highval.txt",@".\discard.txt",@".\output","30" });
+            Directory.Delete(@".\input", true);
+            Directory.Delete(@".\output", true);
+            File.Delete(@".\discard.txt");
+            File.Delete(@".\highval.txt");
+
         }
     }
 }
