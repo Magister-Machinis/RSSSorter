32c85494404cde9744f107e9802a4a580ba2d786
Magister Machinis <30908581+Magister-Machinis@users.noreply.github.com>
2026-01-07 15:55:19 -0500

    updated dotnet versions, added strong name siging, sketched run log class, will do unit tests later

--- ClassBin/Class1.cs
+++ ClassBin/Class1.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.IO;
+using System.IO.Pipelines;
+using System.Linq;
 using System.Net.Http;
-using System.Xml;
+using System.Runtime.InteropServices.Swift;
 using System.ServiceModel.Syndication;
 using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace DataFormats
 {
+    public class RunLogLines
+    {
+        public DateTime TimeStamp { get; set; }
+        public Verbosity Verbosity { get; set; }
+        public string Message { get; set; }
+        public string Source { get; set; }
+    }
+
+    public enum Verbosity
+    {DEBUG, VERBOSE, INFO, NOTICE, WARNING, SUCCESS, ERROR, CRITICAL}
     public struct ResultStatus
     {
         public bool IsSuccess;
@@ -23,6 +42,126 @@ namespace DataFormats
         private string snippet;
     }
 
+    /// <summary>
+    /// class to manage an activity log, has the extra bits needed to save log to file automatically when program is closed
+    /// </summary>
+    public class RunLog : IDisposable
+    {
+        private string logpath;
+        public List<RunLogLines> Log;
+
+        private bool disposed = false;
+        /// <summary>
+        /// Init logger with path to csv formatted log, file does not need to exist yet
+        /// </summary>
+        /// <param name="logpath">path to save log, csv will be created at when first saved if it does not exist</param>
+        public RunLog(string logpath)
+        {
+            this.logpath = logpath;
+            
+            if(File.Exists(this.logpath))
+            {
+                this.Log = this.GetLog().ToList<RunLogLines>();
+            }
+            else
+            {
+                this.Log = new List<RunLogLines>();
+            }
+        }
+        ~RunLog() { Dispose(false); }
+        /// <summary>
+        /// retrieve current log from filepath
+        /// </summary>
+        /// <returns>Logs as array of objects</returns>
+        public RunLogLines[] GetLog()
+        {
+            using (StreamReader sr = new StreamReader(this.logpath))
+            {
+                using (CsvReader csv = new CsvReader(sr, new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    MissingFieldFound = null,
+                    HeaderValidated = null
+                }))
+                {
+
+                    return csv.GetRecords<RunLogLines>().ToArray<RunLogLines>();
+                    
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    this.SaveLog();
+                    disposed = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add new line to log, timestamp is added automatically
+        /// </summary>
+        /// <param name="line">log message</param>
+        /// <param name="source">source of message (interface, scraper, etc)</param>
+        /// <param name="verbosity">verbosity enum</param>
+        public void Writeline(string line, string source, Verbosity verbosity )
+        {
+            this.Log.Add(new RunLogLines()
+            {
+                Message = line,
+                Source = source,
+                Verbosity = verbosity,
+                TimeStamp = DateTime.Now
+            });
+        }
+        /// <summary>
+        /// remove selected line from log
+        /// </summary>
+        /// <param name="linenumber">line to remove</param>
+        public void RemoveLine(int linenumber)
+        {
+            this.Log.RemoveAt(linenumber);
+        }
+        public void SaveLog()
+        {
+            if (File.Exists(this.logpath)) { 
+                File.Delete(this.logpath);
+            }
+            using (StreamWriter writer = new StreamWriter(this.logpath))
+            {
+                using (CsvWriter csvwriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvwriter.WriteRecords(this.Log);
+                }
+            }
+        }
+        /// <summary>
+        /// trim log entries older than set date
+        /// </summary>
+        /// <param name="cuttof">any lines older than this date will be removed from log</param>
+        public void TrimLog (DateTime cuttof)
+        {
+            for(int i =0; i < this.Log.Count; i++)
+            {
+                if (this.Log[i].TimeStamp > cuttof)
+                { 
+                    this.RemoveLine(i);
+                    i--;
+                }
+            }
+        }
+
+    }
     /// <summary>
     /// wrapper class to centralize rss/atom retrieval and parsing
     /// </summary>
---
+++ ClassBinTests/RunLogTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFormats.Tests
+{
+    [TestClass()]
+    public class RunLogTests
+    {
+        [TestMethod()]
+        public void RunLogTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetLogTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WritelineTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveLineTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveLogTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TrimLogTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
